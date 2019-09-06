f945d126ff5f0648894de289050142b36fe4f632
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:49 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,30 @@
+/* eslint-disable flowtype/require-variable-type */
+import mapKeysWithValueKey from "./index.ts";
+
+const upcase = (value) => value.toUpperCase();
+
+test("Object", () => {
+  expect(mapKeysWithValueKey(
+    (value) => (key) => upcase(key + value)
+  )({
+    newLabel: "1",
+    newValue: "2",
+  })).toEqual({
+    NEWLABEL1: "1",
+    NEWVALUE2: "2",
+  });
+});
+
+test("Map", () => {
+  expect(mapKeysWithValueKey(
+    (value) => (key) => upcase(key + value)
+  )(
+    new Map([
+      ["newLabel", "1"],
+      ["newValue", "2"],
+    ])
+  )).toEqual(new Map([
+    ["NEWLABEL1", "1"],
+    ["NEWVALUE2", "2"],
+  ]));
+});
